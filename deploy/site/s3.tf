70fc8adcc5f40d0a60e6188d55827954ec7ba25e
John Hannagan <weetbix_@hotmail.com>
2020-10-18 16:18:21 +0200

    version and purge web build

--- deploy/site/s3.tf
+++ deploy/site/s3.tf
@@ -2,4 +2,15 @@ resource "aws_s3_bucket" "website_bucket" {
   bucket = var.domain_name
   acl    = "private"
   policy = data.aws_iam_policy_document.website_policy.json
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    enabled = true
+    noncurrent_version_expiration {
+      days = 30
+    }
+  }
 }
