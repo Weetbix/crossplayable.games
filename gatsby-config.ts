e3c029cdd96482543aca9e1f3450f32f2e6bf6ed
John Hannagan <weetbix_@hotmail.com>
2020-10-25 11:30:36 +0100

    Use plausible plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,4 +9,14 @@ export const plugins = [
       component: require.resolve(`./src/components/Page.tsx`),
     },
   },
+  {
+    resolve: `gatsby-plugin-plausible`,
+    options: {
+      domain: `crossplayable.games`,
+      // Currently when using a custom domain with self-hosted plausible, the script
+      // path will incorrectly point to index.js. Here we force the correct path...
+      // https://github.com/pixelplicity/gatsby-plugin-plausible/issues/49
+      customDomain: `plausible.cloud.johnhannagan.com/js/plausible.js?original=`,
+    },
+  },
 ];
