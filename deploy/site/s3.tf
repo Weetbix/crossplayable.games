c865b7f28d128d2109802c85b4eca3cc9c7bd44b
John Hannagan <weetbix_@hotmail.com>
2020-10-25 23:20:07 +0100

    fix gzip

--- deploy/site/s3.tf
+++ deploy/site/s3.tf
@@ -18,4 +18,13 @@ resource "aws_s3_bucket" "website_bucket" {
       days = 30
     }
   }
+
+  # Allow cloudfront to get the content-length header
+  # or it will not gzip files
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
 }
