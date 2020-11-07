960b43113cd8b820c14ed151e7f60f04286b42d8
John Hannagan <weetbix_@hotmail.com>
2020-11-07 19:17:27 +0100

    Fix breaking change to playstation webpage

--- src/data/playstationNowGames.ts
+++ src/data/playstationNowGames.ts
@@ -3,20 +3,24 @@ import fetch from "node-fetch";
 import { JSDOM } from "jsdom";
 
 const PSNOW_URL =
-  "https://www.playstation.com/en-us/explore/playstation-now/games/#allgames";
+  "https://www.playstation.com/en-us/ps-now/ps-now-games/#all-ps-now-games";
 
 export const getPlaystationNowGames = async () => {
   const result = await fetch(PSNOW_URL);
   const document = new JSDOM(await result.text()).window.document;
 
   const games = Array.from(
-    document.querySelectorAll("#games-block li.game-title")
+    document.querySelectorAll(".tabs-content p:not(:first-child)")
   )
     .map((titleLi) => titleLi.textContent.trim())
     .map((title) => ({ name: title }));
 
   if (games.length < 800)
-    throw new Error("Lower than expected amount of PSNow games found");
+    throw new Error(
+      `Lower than expected amount of PSNow games found (${
+        games.length
+      } vs min of ${800})`
+    );
 
   return games;
 };
