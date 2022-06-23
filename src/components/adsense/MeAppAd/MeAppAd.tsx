f4cfec2a92910664840c21cc032ec946ca42b414
John Hannagan <weetbix_@hotmail.com>
2022-06-23 23:27:28 +0200

    Add plausible tracking

--- src/components/adsense/MeAppAd/MeAppAd.tsx
+++ src/components/adsense/MeAppAd/MeAppAd.tsx
@@ -16,47 +16,60 @@ const Wrapper = styled.div`
 `;
 
 const LINKS = {
-    DESKTOP: 'https://www.know-yourself.me/',
-    IOS: 'https://apps.apple.com/us/app/me-know-yourself/id1620948275?itsct=apps_box_badge&itscg=30200',
-    ANDROID: 'https://play.google.com/store/apps/details?id=me.knowyourself.app.me'
+    Desktop: 'https://www.know-yourself.me/',
+    iOS: 'https://apps.apple.com/us/app/me-know-yourself/id1620948275?itsct=apps_box_badge&itscg=30200',
+    Android: 'https://play.google.com/store/apps/details?id=me.knowyourself.app.me'
 }
 
 const getPlatformFromUserAgent = () => {
     if(/iPad|iPhone|iPod/.test(navigator.platform)
         || (navigator.userAgent.includes("Mac") && "ontouchend" in document))
     {
-        return "IOS";
+        return "iOS";
     }
 
     if(/android/i.test(navigator.userAgent)) {
-        return "ANDROID";
+        return "Android";
     }
 
-    return "DESKTOP";
+    return "Desktop";
 }
 
 export const MeAppAd = () => {
+    const [plaform, setPlatform] = useState('Desktop');
+    useEffect(() => {
+        setPlatform(getPlatformFromUserAgent());
+    });
 
-  const [link, setLink] = useState(LINKS.DESKTOP);
-  useEffect(() => {
-      setLink(LINKS[getPlatformFromUserAgent()]) 
-  }, [link]);
+    const handleLinkClick = (e) => {
+        e.preventDefault();
 
-  return (
-    <Wrapper>
-        <a href={link}>
-            <picture>
-                <source 
-                    srcSet={`${mobile2x} 2x, ${mobile1x}`}
-                    media="(max-width: 600px)"
-                />
-                <img
-                    srcSet={`${horizontal2x} 2x`}
-                    src={horizontal1x}
-                    alt="Me App banner"
-                />
-            </picture>
-        </a>
-    </Wrapper>
-  );
+        window.plausible?.(
+            'Me App: Clicked',
+            {
+                props: { plaform },
+                callback: () => window.location = LINKS[plaform]
+            }
+        );
+    }
+
+    return (
+        <Wrapper>
+            <a href={LINKS[plaform]} onClick={handleLinkClick}>
+                <picture>
+                    <source 
+                        srcSet={`${mobile2x} 2x, ${mobile1x}`}
+                        media="(max-width: 600px)"
+                    />
+                    <img
+                        srcSet={`${horizontal2x} 2x`}
+                        src={horizontal1x}
+                        alt="Me App banner"
+                        loading="lazy"
+                        onLoad={() => window.plausible?.('Me App: Viewed', { props: { plaform }})}
+                    />
+                </picture>
+            </a>
+        </Wrapper>
+    );
 };
